0bc56cafe2c9b8459311886953aab48acfe5f2f3
Oscar A Castro <oacastro2@gmail.com>
2020-09-03 23:50:31 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -25,4 +25,5 @@ def count_empty_strings(array)
     end 
       
      
-end       # Return the total number of EMPTY strings in the provided array using the count enumerable
+end       
+# Return the total number of EMPTY strings in the provided array using the count enumerable
